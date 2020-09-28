4d34a97e742729f54f78b2c00a1fb9e49332834a
LysakArtem <Perry.dev7@gmail.com>
2020-09-28 22:31:54 +0300

    first commit

---
+++ js/index.js
@@ -0,0 +1,29 @@
+
+const targetDate=new Date('May 22, 2021').getTime()
+console.log(targetDate);
+const daysSpanRef=document.querySelector('[data-value="days"]');
+const hoursSpanRef=document.querySelector('[data-value="hours"]');
+const minsSpanRef=document.querySelector('[data-value="mins"]');
+const secsSpanRef=document.querySelector('[data-value="secs"]');
+
+
+setInterval(()=>{
+const currerntDate=Date.now();
+const deltaTime= targetDate-currerntDate;
+updateClockFace(deltaTime);
+},1000);
+
+function updateClockFace(time){
+const days = Math.floor(time / (1000 * 60 * 60 * 24));
+const hours = pad(Math.floor((time % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60)));
+const mins = pad(Math.floor((time % (1000 * 60 * 60)) / (1000 * 60)));
+const secs = pad(Math.floor((time % (1000 * 60)) / 1000));
+daysSpanRef.textContent=days;
+hoursSpanRef.textContent=hours;
+minsSpanRef.textContent=mins;
+secsSpanRef.textContent=secs;
+}
+
+function pad(value){
+    return String(value).padStart(2,'0')
+}
